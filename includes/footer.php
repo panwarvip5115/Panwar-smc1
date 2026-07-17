f3bbcd5810d7ed26c0d40fe96d46fda7fdcb4c35
panwarvip5115 <v8435994@gmail.com>
2026-07-18 02:20:25 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer style="background:#111;color:white;text-align:center;padding:20px;margin-top:50px;">
+    <h3>Panwar SMC WITH NEW CONCEPT</h3>
+    <p>© 2026 Aditya Jangid. All Rights Reserved.</p>
+</footer>
+
+<script src="assets/js/script.js"></script>
+
+</body>
+</html>
