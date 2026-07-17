dd1ee957ffe2eab498af1436ec6a14e923ff7cf1
panwarvip5115 <v8435994@gmail.com>
2026-07-18 02:16:26 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Panwar SMC WITH NEW CONCEPT</title>
+</head>
+<body>
+
+<h1>Welcome to Panwar SMC WITH NEW CONCEPT</h1>
+
+<p>Website is under development...</p>
+
+</body>
+</html>
