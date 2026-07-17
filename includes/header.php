d278f3757ef2ef6cd08f1177e1cb08a0223adcde
panwarvip5115 <v8435994@gmail.com>
2026-07-18 02:36:08 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Panwar SMC</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>FX Panwar</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <link rel="stylesheet" href="assets/css/style.css">
+
 </head>
 
 <body>
