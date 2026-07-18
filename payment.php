d392d3fd6404ffab04a8e13421f5432f0de0366d
panwarvip5115 <v8435994@gmail.com>
2026-07-18 15:15:49 +0530

    Create payment.php

---
+++ payment.php
@@ -0,0 +1,21 @@
+<?php
+include 'includes/header.php';
+?>
+
+<div class="container mt-5 text-center">
+    <h1 class="text-warning">Buy Course</h1>
+    <h3>Panwar SMC WITH NEW CONCEPT</h3>
+    <h2>₹14,999</h2>
+
+    <img src="assets/images/qr.png" width="300">
+
+    <br><br>
+
+    <a href="#" class="btn btn-warning">
+        Upload Payment Screenshot
+    </a>
+</div>
+
+<?php
+include 'includes/footer.php';
+?>
