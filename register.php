5edba24b5c268568790baf14468b3b1e28909064
panwarvip5115 <v8435994@gmail.com>
2026-07-18 15:17:28 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,32 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container mt-5" style="max-width:500px;">
+
+<h2 class="text-warning text-center">Student Registration</h2>
+
+<form action="#" method="POST">
+
+<div class="mb-3">
+<label>Full Name</label>
+<input type="text" class="form-control" name="name" required>
+</div>
+
+<div class="mb-3">
+<label>Email</label>
+<input type="email" class="form-control" name="email" required>
+</div>
+
+<div class="mb-3">
+<label>Password</label>
+<input type="password" class="form-control" name="password" required>
+</div>
+
+<button class="btn btn-warning w-100">
+Create Account
+</button>
+
+</form>
+
+</div>
+
+<?php include 'includes/footer.php'; ?>
