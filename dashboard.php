ade96f19f692d30546d2b4b467e62b6717e8a1f9
panwarvip5115 <v8435994@gmail.com>
2026-07-18 15:19:26 +0530

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,41 @@
+<?php
+include 'includes/header.php';
+?>
+
+<div class="container mt-5">
+
+<h1 class="text-warning">Welcome to FX Panwar</h1>
+
+<p>Hello Student 👋</p>
+
+<div class="row mt-4">
+
+<div class="col-md-4">
+<div class="card bg-dark text-white p-3">
+<h3>My Course</h3>
+<p>Panwar SMC WITH NEW CONCEPT</p>
+<a href="#" class="btn btn-warning">Watch Course</a>
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="card bg-dark text-white p-3">
+<h3>Payment</h3>
+<p>Status: Pending</p>
+</div>
+</div>
+
+<div class="col-md-4">
+<div class="card bg-dark text-white p-3">
+<h3>Support</h3>
+<a href="#" class="btn btn-success">Join Telegram</a>
+</div>
+</div>
+
+</div>
+
+</div>
+
+<?php
+include 'includes/footer.php';
+?>
